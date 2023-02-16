f5207736609c5d9a34c5bf8fbe71928cf58f91cb
carissia <reyecari@kean.edu>
2023-02-15 21:23:51 -0500

    lab3

---
+++ Maximum/CPS3330/MyMaximumLibTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+using CalculateMaximum;
+using MyMaximumLibTest;
+namespace MyMaximumLibTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestMaximum() {
+        double a = 4;
+        double b = 2;
+        double actual = MyMaxmethod.MyMax(a, b);
+        
+    }
+    [Fact]
+    public void TestMaximum1() 
+    {
+        double a = 2;
+        double b = 4;
+        double c = 6;
+        
+        double actual = MyMaxmethod.MyMax1(a, b ,c);
+     
+
+    } 
+
+    
+}
